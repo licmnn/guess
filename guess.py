400813f794f53a7d218ef76a671c568e87ec8a49
licmnn <licmn@live.com>
2020-05-20 08:59:23 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('请决定随机数字范围开始值： ') #由用户确定起始值
+end = input('请决定随机数字范围结束值： ')   #由用户确定结束值
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0             # 设定计数，猜了几次
 while True:
 	count += 1         #增加计数器
